be4559ef04bf7637a863c0a08e7355debaab8088
Carlos Martínez <carmargut@gmail.com>
2024-09-26 17:54:42 +0200

    chore: First commit with basic structure

---
+++ src/test/java/com/retail/dynamic_pricer_service/DynamicPricerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.retail.dynamic_pricer_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynamicPricerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
