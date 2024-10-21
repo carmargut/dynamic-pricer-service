984b1bf6205fc2405cf1974ceaf3c4d1941db1d6
Carlos Martínez <carmargut@gmail.com>
2024-10-21 02:47:32 +0200

    refactor: Add exception hander tests

--- src/main/java/com/retail/dynamic_pricer_service/infrastructure/apis/rest/RestErrorResponse.java
+++ src/main/java/com/retail/dynamic_pricer_service/infrastructure/apis/rest/RestErrorResponse.java
@@ -1,9 +1,5 @@
 package com.retail.dynamic_pricer_service.infrastructure.apis.rest;
 
-import java.time.LocalDateTime;
+public record RestErrorResponse(String message, String status) {
 
-public record RestErrorResponse(String message, String status, LocalDateTime timestamp) {
-    public RestErrorResponse(String message, String status) {
-        this(message, status, LocalDateTime.now());
-    }
 }
